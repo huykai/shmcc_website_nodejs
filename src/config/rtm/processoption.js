b7cc8cc5c3c53d5af27aefca61922469e89a0026
huykai <huykailrui@gmail.com>
2018-12-14 18:04:33 -0800

    20181214

--- src/config/rtm/processoption.js
+++ src/config/rtm/processoption.js
@@ -16,7 +16,7 @@ exports.env = {
   secretToken: 'aMdoeb5ed87zorRdkD6greDML81DcnrzeSD648ferFejmplx',
   socketPort: 3100,
   inspectClient: {
-    script: '/root/huykai/javascript_projects/IntelligentInspection',
+    script: '/root/huykai/javascript_projects/IntelligentInspection/InspectWorker.js',
     args: [
       'yml/WorkerConfigRtm.yml'
     ]
